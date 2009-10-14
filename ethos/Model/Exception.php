2f06435044887dee3f862eb4f0a2083537382051
David Rogers <david@ethos-development.com>
2009-10-14 00:48:14 -0400

    Adding an appropriate Exception class for the Model package: ethos_Model_Exception

---
+++ ethos/Model/Exception.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @license GPL3.0?
+ * @author David Rogers <david@ethos-development.com>
+ * @package ethos_Model
+ * @category Exceptions
+ */
+
+/**
+ * @license GPL3.0?
+ * @author David Rogers <david@ethos-development.com>
+ * @package ethos_Model
+ * @category Exceptions
+ */
+class ethos_Model_Exception
+extends ethos_Exception
+{
+
+} // END ethos_Model_Exception
